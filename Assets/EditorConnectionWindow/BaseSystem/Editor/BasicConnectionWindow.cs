188ff61cf596747f970f0ce76f886dd9302660d5
Friedrich Wessel <friedrich.wessel@innogames.de>
2017-11-12 14:03:03 +0100

    Initial commit with empty window

---
+++ Assets/EditorConnectionWindow/BaseSystem/Editor/BasicConnectionWindow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+public class BasicConnectionWindow : EditorWindow {
+
+    // Add menu named "My Window" to the Window menu
+    [MenuItem("Tools/ConnectionWindow")]
+    static void Init()
+    {
+        // Get existing open window or if none, make a new one:
+        var window = (BasicConnectionWindow)EditorWindow.GetWindow(typeof(BasicConnectionWindow));
+        window.Show();
+    }
+}
