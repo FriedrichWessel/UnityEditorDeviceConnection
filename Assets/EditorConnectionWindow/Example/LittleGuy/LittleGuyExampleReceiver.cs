3e96a5bfa1be2f7c511fbf121531fcf4ad0e4f8b
Friedrich Wessel <friedrich.wessel@innogames.com>
2017-12-14 22:25:01 +0100

    Add new Example implementation
    3d Scene with little Guy now has an example to receive Cheats

---
+++ Assets/EditorConnectionWindow/Example/LittleGuy/LittleGuyExampleReceiver.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using EditorConnectionWindow.BaseSystem;
+using UnityEngine;
+
+public class LittleGuyExampleReceiver : MonoBehaviour
+{
+	[SerializeField] private LittleGuyView View; 
+	
+	private EditorConnectionServer _server;
+
+	// Use this for initialization
+	void Start ()
+	{
+		_server = new EditorConnectionServer(8080, 15000);
+		_server.DataReceived += SendCommandToLittleGuy;
+		_server.StartServer();
+	}
+
+	private void SendCommandToLittleGuy(string command)
+	{
+		switch (command)
+		{
+			case "Walk": 
+				View.StartWalkAnimation();
+				break;
+			case "Idle": 
+				View.StartIdleAnimation();
+				break;
+			default:
+				Debug.Log(string.Format("Command {0} not available", command));
+				break;
+				
+		}
+	}
+
+	private void OnDestroy()
+	{
+		_server.StopServer();
+	}
+
+	void Update () {
+		_server.Tick();
+	}
+}
